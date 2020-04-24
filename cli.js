c85a8aed06eb652b028e77f9eb6f291e9427f4eb
Riccardo Bellingeri <rbellingeri@breadfinance.com>
2020-04-24 17:12:06 -0400

    Add package version

--- cli.js
+++ cli.js
@@ -1,3 +1,6 @@
 #!/usr/bin/env node
+const { version } = require("./package.json");
 
-console.log("Hello world! This is a GitHub package installed with npm.");
+console.log(
+  `Hello world! This is version \`${version}\` of a GitHub package installed with npm.`
+);
